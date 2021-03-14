58ecd79c48ceeeaaceb873a0dd1f6f12de3c886d
Berthold Metz <bnetz@gmx.de>
2021-03-14 14:40:37 +0100

    merged

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Meine Website!!</title>
+  <meta name="description" content="Meine Website, Beschreibung">
+  <meta name="author" content="Kommissar Smith">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  echo "blalllll";
+  <script src="js/scripts.js"></script>
+  
+</body>
+</html>
